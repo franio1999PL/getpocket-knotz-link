f376cdb182d6b304c39f034c1f59a61e0845ccd7
Franciszek Sikora <sikorafranek@gmail.com>
2023-01-08 17:45:04 +0100

    update and added categories path

--- server.js
+++ server.js
@@ -1,61 +1,104 @@
-import auth from 'pocket-auth'
-import express from 'express'
-import cors from 'cors'
-import GetPocket from 'node-getpocket'
-import dotenv from 'dotenv'
+const auth = require('pocket-auth')
+const express = require('express')
+const cors = require('cors')
+const GetPocket = require('node-getpocket')
+const dotenv = require('dotenv')
+const sgMail = require('@sendgrid/mail')
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
+
+// Load instance of express app
 const app = express()
 app.use(cors())
 
-var consumerKey = process.env.CONSUMER_KEY
-var redirectUri = 'https://google.com'
-
-auth.fetchToken(consumerKey, redirectUri, {}, function (err, code) {
-  let uri = auth.getRedirectUrl(code.code, redirectUri)
-  console.log(
-    'Visit the following URL and click approve in the next 60 seconds:'
-  )
-  console.log(uri)
-
-  setTimeout(async function () {
-    auth.getAccessToken(consumerKey, code.code, function (err, r) {
-      if (err) {
-        console.log(
-          "You didn't click the link and approve the application in time"
-        )
-        return
-      }
-
-      //start code
-      const { access_token } = r
-
-      const config = {
-        consumer_key: process.env.CONSUMER_KEY,
-        access_token: access_token
-      }
-      const pocket = new GetPocket(config)
-
-      const params = {
-        // get/retrieve/search parameters
-        favorite: 1,
-        count: 1000,
-        sort: 'newest'
-      }
-      pocket.get(params, (err, resp) => {
-        if (err) {
-          console.log(err)
-          return
-        }
-        return resp
-      })
-    })
-  }, 10000)
-})
+// Konfiguracja SENDGRID
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 app.get('/', (req, res) => {
+  // Konfiuracja do pobrania artykułów z getpocket api
+  const config = {
+    consumer_key: process.env.CONSUMER_KEY,
+    access_token: process.env.ACCESS_KEY
+  }
+
+  const pocket = new GetPocket(config)
+
+  const params = {
+    favorite: 1,
+    count: 1000,
+    sort: 'newest',
+    detailType: 'complete'
+  }
+  pocket.get(params, (err, resp) => {
+    if (err) {
+      console.log(err)
+
+      const emailNotification = {
+        to: 'sikorafranek@gmail.com',
+        from: 'no-reply@blady.dev',
+        subject: 'Problem z API knotz.link',
+        text: `
+          Problem z autoryzacja access_key do api getpocket.com
+          
+          Pełny error msg: ${err}
+        `,
+        html: `<strong>
+        Problem z autoryzacja access_key do api getpocket.com
+        
+        Pełny error msg: ${err}</strong>
+      `
+      }
+
+      sgMail
+        .send(emailNotification)
+        .then(response => {
+          // console.log(response[0].statusCode)
+          // console.log(response[0].headers)
+          console.log('Wysłano wiadomość informacyjną o problemie z API')
+        })
+        .catch(error => {
+          console.error(error)
+        })
+
+      res.status(401).send({
+        error: '401',
+        message: 'Error with authorization (unknown access token)'
+      })
+      return
+    }
+
+    const items = resp.list
+    const modifiedData = Object.values(items).map(
+      ({
+        item_id: id,
+        given_url: url,
+        resolved_title: title,
+        excerpt: description,
+        time_favorited: time_added,
+        time_to_read: read_time,
+        word_count,
+        tags
+      }) => ({
+        id,
+        url,
+        title,
+        description,
+        time_added,
+        read_time,
+        word_count,
+        tags
+      })
+    )
+    res.send(modifiedData)
+  })
+})
+
+app.get('/categories', (req, res) => {
+  const tag = req.query.tag
+  const count = req.query.count
+
   const config = {
     consumer_key: process.env.CONSUMER_KEY,
     access_token: process.env.ACCESS_KEY
@@ -63,10 +106,11 @@ app.get('/', (req, res) => {
   const pocket = new GetPocket(config)
 
   const params = {
-    // get/retrieve/search parameters
     favorite: 1,
-    count: 1000,
-    sort: 'newest'
+    tag,
+    count
+    // sort: 'newest',
+    // detailType: 'complete'
   }
   pocket.get(params, (err, resp) => {
     if (err) {
@@ -76,12 +120,24 @@ app.get('/', (req, res) => {
 
     const items = resp.list
     const modifiedData = Object.values(items).map(
-      ({ item_id, has_image, given_url, resolved_title, excerpt }) => ({
-        item_id,
-        has_image,
-        given_url,
-        resolved_title,
-        excerpt
+      ({
+        item_id: id,
+        given_url: url,
+        resolved_title: title,
+        excerpt: description,
+        time_favorited: time_added,
+        time_to_read: read_time,
+        word_count,
+        tags
+      }) => ({
+        id,
+        url,
+        title,
+        description,
+        time_added,
+        read_time,
+        word_count,
+        tags
       })
     )
     res.send(modifiedData)
